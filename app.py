e1b7fec9890ef133a840b1d9130dc6fcf2060cac
Rei Nelku <reinelku@gmail.com>
2023-11-16 16:13:28 +0100

    Create Flask Config

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)

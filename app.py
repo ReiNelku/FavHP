ca8b710189163b744d8d84206f9bb62f28ccb95d
Rei Nelku <reinelku@gmail.com>
2023-11-17 15:47:48 +0100

    Create register route

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_session import Session
 # Configure application
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        ...
+    else:
+        return render_template("register.html")

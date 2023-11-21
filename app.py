9322bdc5b0e44f97ab744a4c2558cb84e4b20072
Rei Nelku <reinelku@gmail.com>
2023-11-21 16:36:48 +0100

    Configure database in Flask

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, redirect, render_template, request, session
+from cs50 import SQL
+from flask import abort, Flask, redirect, render_template, request, session
 from flask_session import Session
 
 # Configure application
 app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite databse
+db = SQL("sqlite:///favhp.db")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():

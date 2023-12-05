8e1ae35f1d3cef3f7e8c0d53a4d84b005f5fb308
Rei Nelku <reinelku@gmail.com>
2023-12-05 17:02:58 +0100

    Form submittion check in Register

--- app.py
+++ app.py
@@ -11,6 +11,23 @@ db = SQL("sqlite:///favhp.db")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        ...
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Check if username is submitted
+        if not username:
+            return render_template("error.html", code="400", reason="must-provide-username")
+        # Check if email is submitted    
+        elif not email:
+            return render_template("error.html", code="400", reason="must-provide-email")
+        # Check if password is submitted    
+        elif not password:
+            return render_template("error.html", code="400", reason="must-provide-password")
+        # Check if password confirmation is submitted
+        elif not confirmation:
+            return render_template("error.html", code="400", reason="must-type-your-password-again")
+        
     else:
         return render_template("register.html")

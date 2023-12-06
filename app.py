bcc342abaeec79bf02e05db284e7386095b90fa1
Rei Nelku <reinelku@gmail.com>
2023-12-06 18:32:31 +0100

    Create login route

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Configure application
 app = Flask(__name__)
 
-# Configure CS50 Library to use SQLite databse
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///favhp.db")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -51,3 +56,14 @@ def register():
         return redirect("/")
     else:
         return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+        ...
+    else:
+        return render_template("login.html")

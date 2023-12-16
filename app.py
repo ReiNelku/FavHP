e18c5fe8faaa51dbf0251b72528ed62cbef6fdf6
Rei Nelku <reinelku@gmail.com>
2023-12-16 02:59:41 +0100

    Complete Login route

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def register():
             return render_template("error.html", code="400", reason="passwords-do-not-match")
 
         # Create a new user in database
-        user = db.execute("INSERT INTO users (username, email, hash) VALUES(?, ?, ?)", username, email, generate_password_hash("password"))
-
+        user = db.execute("INSERT INTO users (username, email, hash) VALUES(?, ?, ?)", username, email, generate_password_hash(password))
+        
         # Log in with the newly created user
         session["user_id"] = id
 
@@ -63,7 +63,28 @@ def login():
     # Forget any user_id
     session.clear()
 
+    namemail = request.form.get("namemail")
+    password = request.form.get("password")
+
     if request.method == "POST":
-        ...
+        # Check if username or email is submitted
+        if not namemail:
+            return render_template("error.html", code="403", reason="must-provide-username-or-email")
+        # Check if password is submitted
+        elif not password:
+            return render_template("error.html", code="403", reason="must-provide-password")
+        
+        # Query database for user
+        user = db.execute("SELECT * FROM users WHERE username = ? OR email = ?", namemail, namemail)
+
+        # Ensure user exists and password is correct
+        if len(user) != 1 or not check_password_hash(user[0]["hash"], password):
+            return render_template("error.html", code="403", reason="invalid-username,email-and~sor-password")
+
+        # Remember logged in user
+        session["user_id"] = user[0]["id"]
+
+        # Redirect user to homepage
+        return redirect("/")
     else:
         return render_template("login.html")

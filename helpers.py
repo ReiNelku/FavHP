2150eb3432dc84200e690acdf547c5febeb678c2
Rei Nelku <reinelku@gmail.com>
2023-12-16 14:26:59 +0100

    Create login required decorator

---
+++ helpers.py
@@ -0,0 +1,8 @@
+# Decorate routes to require login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
